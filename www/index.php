804e77debcc8ed7caf3753350f39819591c85295
gangavh@gmail.com <gangavh@gmail.com>
2017-12-16 01:20:55 -0600

    Updated the stuff

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world!</title>
+	<title>2017 Mindtree Hackathon 2017!!!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
